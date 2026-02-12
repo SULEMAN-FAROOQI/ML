1faea8a1aa2bfa2c6eee7c348131c4f2fd0af16c
Suleman <sulemanhf06@gmail.com>
2026-02-12 21:13:36 +0500

    #.

--- ALGORITHMS/Isolation Forest.py
+++ ALGORITHMS/Isolation Forest.py
@@ -2,19 +2,15 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.ensemble import IsolationForest
-from sklearn.datasets import fetch_covtype
 
-data = fetch_covtype(as_frame=True)
+x = np.random.randn(100, 2)
 
-x = data.data
-y = data.target
-
-iforest = IsolationForest(contamination=0.3)
+iforest = IsolationForest(contamination=0.1)
 forest_labels = iforest.fit_predict(x) 
 
 # -1 for anomaly, 1 for normal
 
-plt.scatter(x.iloc[:,0], x.iloc[:,1], c = forest_labels, cmap="viridis")
+plt.scatter(x[:,0], x[:,1], c = forest_labels, cmap="viridis")
 plt.show()
 
 # pip install eif remember search when it can support python 3.13
