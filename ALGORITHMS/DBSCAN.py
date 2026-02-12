4541134e7053a6c21be5ab250ddceb8ab5b46817
Suleman <sulemanhf06@gmail.com>
2026-02-12 20:59:48 +0500

    #.

--- ALGORITHMS/DBSCAN.py
+++ ALGORITHMS/DBSCAN.py
@@ -8,7 +8,7 @@ x,y = make_moons(n_samples=200, noise=0.3, random_state=42)
 dbscan = DBSCAN(eps=0.3, min_samples=5)
 labels = dbscan.fit_predict(x)
 
-# Agglomerative clustering is unsupervised, so it does not use y
+# Agglomerative clustering is unsupervised, so it does not use y. It can also be used for anamoly detection.
 
 plt.scatter(x[:,0], x[:,1], c=labels, cmap="viridis")
 plt.show()
