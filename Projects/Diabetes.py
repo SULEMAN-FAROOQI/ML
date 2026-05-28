8a568c2e1f527d0fe6d887a1ca29c9516983a340
Suleman <sulemanhf06@gmail.com>
2026-05-28 21:48:34 +0500

    #.

--- Projects/Diabetes.py
+++ Projects/Diabetes.py
@@ -8,7 +8,10 @@ from sklearn.pipeline import make_pipeline
 from sklearn.compose import make_column_transformer
 from sklearn.model_selection import train_test_split , cross_val_score , GridSearchCV
 from sklearn.metrics import accuracy_score
-from sklearn.linear_model import LogisticRegression
+from xgboost import XGBClassifier
+from sklearn.experimental import enable_iterative_imputer
+from sklearn.impute import IterativeImputer
+from sklearn.ensemble import ExtraTreesRegressor
 import warnings
 warnings.filterwarnings('ignore')
 
@@ -23,6 +26,7 @@ x = df.drop("Diagnosis", axis = 1)
 y = df["Diagnosis"]
 
 columnx = x.columns.tolist()
+df[['Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI']].replace(0, np.nan)
 
 # Plotting each column to check linearity
 
@@ -42,7 +46,11 @@ for i in range(x.shape[1] - 1):
 trainx , testx , trainy , testy = train_test_split(x,y , stratify=y, random_state=33, test_size=0.3)
 
 z = make_column_transformer(
-    [StandardScaler() , columnx], 
+    (make_pipeline(
+        IterativeImputer(estimator=ExtraTreesRegressor(n_estimators=10, random_state=33), 
+                 max_iter=10, random_state=33),
+        StandardScaler()
+    ), columnx),
     remainder="passthrough"
 )
 
@@ -51,18 +59,21 @@ z = make_column_transformer(
 '''
 
 param = {
-    'C': [0.1, 1, 10],
-    'penalty': ['l1', 'l2'],
-    'solver': ['sag','saga', 'liblinear'], # Supports both l1 and l2
-    'max_iter': [1000]       # Ensures it converges
+    'n_estimators':     [150, 200, 250],
+    'max_depth':        [4, 5, 6],          # tighter around 5
+    'learning_rate':    [0.005, 0.01, 0.05], # tighter around 0.01
+    'subsample':        [0.8, 0.9, 1.0],
+    'colsample_bytree': [0.8, 0.9, 1.0],
+    'gamma':            [0, 0.05, 0.1]       # tighter around 0
 }
 
 grid = GridSearchCV(
-    LogisticRegression(),
+    XGBClassifier(use_label_encoder=False, eval_metric='logloss', random_state=33),
     param,
     cv=5,
     scoring="accuracy",
-    n_jobs=-1
+    n_jobs=-1,
+    verbose=1
 )
 
 grid.fit(trainx, trainy)
@@ -72,7 +83,17 @@ print("CV Accuracy:", grid.best_score_)
 
 '''
 
-m = LogisticRegression(C =  1, max_iter = 1000, penalty = 'l1', solver = 'liblinear')
+m = XGBClassifier(
+    colsample_bytree  = 1.0,
+    gamma             = 0.1,  
+    learning_rate     = 0.01,
+    max_depth         = 5,
+    n_estimators      = 150,   
+    subsample         = 1.0,
+    use_label_encoder = False,
+    eval_metric       = 'logloss',
+    random_state      = 33
+)
 
 pipeline = make_pipeline(z,m)
 
@@ -94,5 +115,5 @@ new_data = pd.DataFrame([{
 
 prediction = pipeline.predict(new_data)
 
-result = "Negative" if prediction[0] == '0' else "Positive"
+result = "Negative" if prediction[0] == 0 else "Positive"
 print("Prediction:", result)
