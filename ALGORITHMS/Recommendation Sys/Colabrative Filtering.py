bc1562018768910c6aa0c92b12bddfbbfa2428e1
Suleman Farooqi <sulemanhf06@gmail.com>
2026-06-19 21:10:25 +0500

    #.

--- ALGORITHMS/Recommendation Sys/Colabrative Filtering.py
+++ ALGORITHMS/Recommendation Sys/Colabrative Filtering.py
@@ -2,9 +2,12 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-books = pd.read_csv("Datasets\\BookRecomData\\Books.csv")
-ratings = pd.read_csv("Datasets\\BookRecomData\\Ratings.csv")
-users = pd.read_csv("Datasets\\BookRecomData\\Users.csv")
+books = pd.read_csv("Datasets\\BookRecomData\\Books.csv", low_memory=False)
+ratings = pd.read_csv("Datasets\\BookRecomData\\Ratings.csv", low_memory=False)
+users = pd.read_csv("Datasets\\BookRecomData\\Users.csv", low_memory=False)
 
-# Merge the datasets
-
+print(books.isnull().sum())
+print("-")
+print(ratings.isnull().sum())
+print("-")
+print(users.isnull().sum())
