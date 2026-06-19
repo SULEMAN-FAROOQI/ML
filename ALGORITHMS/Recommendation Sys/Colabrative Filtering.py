1eb9c6039f4a73158bfe57e33592ca6d1a776a56
Suleman Farooqi <sulemanhf06@gmail.com>
2026-06-19 16:28:16 +0500

    #.

--- ALGORITHMS/Recommendation Sys/Colabrative Filtering.py
+++ ALGORITHMS/Recommendation Sys/Colabrative Filtering.py
@@ -5,3 +5,4 @@ books = pd.read_csv("Datasets\\BookRecomData\\Books.csv")
 ratings = pd.read_csv("Datasets\\BookRecomData\\Ratings.csv")
 users = pd.read_csv("Datasets\\BookRecomData\\Users.csv")
 
+# Merge the datasets
