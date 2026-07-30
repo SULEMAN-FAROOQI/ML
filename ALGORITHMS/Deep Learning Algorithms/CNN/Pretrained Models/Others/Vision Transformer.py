1c2dc6b03fa7c3e51c890156590047e425d7653f
Suleman Farooqi <sulemanhf06@gmail.com>
2026-07-30 20:03:27 +0500

    Yokoso

--- ALGORITHMS/Deep Learning Algorithms/CNN/Pretrained Models/Others/Vision Transformer.py
+++ ALGORITHMS/Deep Learning Algorithms/CNN/Pretrained Models/Others/Vision Transformer.py
@@ -6,14 +6,17 @@ os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"     # Suppress INFO, WARNING, and ERROR
 
 import keras
 import keras_hub
+import numpy as np
 
 classifier = keras_hub.models.ImageClassifier.from_preset(
-    "vit_base_patch16_224_imagenet"
+    "vit_base_patch16_224_imagenet",
+    preprocessor=None,  # skips the object that requires tf-text
 )
 
 img = keras.utils.load_img("Datasets\\Cats vs Dogs\\Final_test\\dog0.jpg", target_size=(224, 224))
 x = keras.utils.img_to_array(img)
+x = np.expand_dims(x, axis=0)
+x = x / 255.0  
 
-preds = classifier.predict([x])
-print("\n")
+preds = classifier.predict(x)
 print(preds)
